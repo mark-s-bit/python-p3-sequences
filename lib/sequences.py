ca32bffa650f8961aa2f079b0118b35d2f676ad4
mark-s-bit <markchege03@gmail.com>
2024-09-05 11:53:04 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    # Start with the first two Fibonacci numbers
+    fibonacci_sequence = [0, 1]
+    
+    while len(fibonacci_sequence) < length:
+        # Append the sum of the last two numbers in the list
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    
+    # If length is 1, return only the first element
+    if length == 1:
+        fibonacci_sequence = fibonacci_sequence[:1]
+    
+    print(fibonacci_sequence)
+
+pass
